4e8a6c0ce387828ec09801e19d6b30ac1c5a5789
Alysson M <almenezes912@gmail.com>
2022-11-10 19:58:35 -0300

    commitando projeto inicial

---
+++ PrimeiroProjetoTabBar/PrimeiroProjetoTabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PrimeiroProjetoTabBar
+//
+//  Created by ALYSSON MENEZES on 10/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
